8f6de957825efc3422bb3c5e8899d397489c1a32
Chi Ho <>
2020-07-13 19:48:55 +0200

    removed unused interface props

--- src/components/DetailPage/DetailPage.tsx
+++ src/components/DetailPage/DetailPage.tsx
@@ -5,8 +5,6 @@ import ReactMarkdown from "react-markdown";
 
 interface StateToProps {
     cast: any;
-    summary: any;
-    pageSecured: boolean;
 }
 
 const Result: React.FC<StateToProps> = ({ cast }) => {
@@ -32,7 +30,7 @@ const Result: React.FC<StateToProps> = ({ cast }) => {
 }
 
 const mapStateToProps = (state: any) => {
-    console.log(state.catalogData.showDetail)
+    console.log(state)
     return {
         cast: state.catalogData.castList || [],
         summary: state.catalogData.catalogInfo.data || [],
