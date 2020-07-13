a6755def989a63fa130eef6992fb0a5a5b6860d8
Chi Ho <>
2020-07-13 19:49:42 +0200

    action getCast added

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -1,10 +1,18 @@
 export const types = {
-    GET_DATA: 'GET_DATA'
+    GET_DATA: 'GET_DATA',
+    GET_CAST: 'GET_CAST'
 }
 
 export const getData = (data: object) => {
     return {
         type: types.GET_DATA,
         data
+    }
+}
+
+export const getCast = (castData: object) => {
+    return {
+        type: types.GET_CAST,
+        castData
     }
 }
